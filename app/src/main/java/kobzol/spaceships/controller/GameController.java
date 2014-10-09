0b321e5b612e00d86fc6c22d109e5755b24884c6
Kobzol <berykubik@gmail.com>
2014-10-09 23:29:09 +0200

    added class GameController

---
+++ app/src/main/java/kobzol/spaceships/controller/GameController.java
@@ -0,0 +1,9 @@
+package kobzol.spaceships.controller;
+
+/**
+ * Creates and controls the game.
+ *
+ * GameController runs the main game loop and contains all of the game objects.
+ */
+public class GameController {
+}
