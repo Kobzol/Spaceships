b6c86e67c5f919cf8d2d2f8cbec6e7e9b1e9cd2d
Kobzol <berykubik@gmail.com>
2014-10-11 00:34:21 +0200

    added class DisplayHelper

---
+++ app/src/main/java/kobzol/spaceships/ui/DisplayHelper.java
@@ -0,0 +1,22 @@
+package kobzol.spaceships.ui;
+
+import android.content.Context;
+import android.util.DisplayMetrics;
+import android.view.WindowManager;
+
+/**
+ * Static class with display methods.
+ */
+public class DisplayHelper {
+    /**
+     * Gets the display metrics for the given device.
+     * @param context context
+     * @return display metrics
+     */
+    public DisplayMetrics getDisplayMetrics(Context context) {
+        DisplayMetrics metrics = new DisplayMetrics();
+        ((WindowManager) context.getSystemService(Context.WINDOW_SERVICE)).getDefaultDisplay().getMetrics(metrics);
+
+        return metrics;
+    }
+}
