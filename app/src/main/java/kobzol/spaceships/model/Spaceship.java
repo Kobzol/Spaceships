2af97915cec0d7e1b5661309688e06190dfdc1ac
Kobzol <berykubik@gmail.com>
2014-10-11 21:21:56 +0200

    added class Spaceship and an image that can be used for the player's spaceship

---
+++ app/src/main/java/kobzol/spaceships/model/Spaceship.java
@@ -0,0 +1,13 @@
+package kobzol.spaceships.model;
+
+import android.graphics.Bitmap;
+
+/**
+ * Spaceship.
+ */
+public class Spaceship extends DisplayableObject {
+    public Spaceship(Bitmap bitmap) {
+        super(bitmap);
+    }
+
+}
