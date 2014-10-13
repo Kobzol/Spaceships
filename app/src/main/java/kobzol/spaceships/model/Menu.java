bdefe3ad1e23689d9b7e51b4fc8ae2749ba20abf
Kobzol <berykubik@gmail.com>
2014-10-13 07:53:13 +0200

    added class Menu

---
+++ app/src/main/java/kobzol/spaceships/model/Menu.java
@@ -0,0 +1,10 @@
+package kobzol.spaceships.model;
+
+/**
+ * Contains buttons that control the game.
+ */
+public class Menu extends DisplayableObject {
+    public Menu(Dimension dimension, float speed) {
+        super(dimension, speed);
+    }
+}
